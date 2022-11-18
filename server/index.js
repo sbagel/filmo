9364c22032abe2423551c615f160a15e41d158e3
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2022-11-18 08:32:17 -0800

    unneeded packages, s3

--- server/index.js
+++ server/index.js
@@ -2,15 +2,14 @@ const dotenv = require('dotenv').config()
 const express = require('express');
 const app = express();
 const morgan = require('morgan')
-const PORT = process.env.PORT;
 const router = require('./routes.js');
 const db = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true} ));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use('/api', router)
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}/api`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on http://localhost:${process.env.PORT}/api`);
 })
